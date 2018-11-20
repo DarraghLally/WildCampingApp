06762e1dc29dcdeaf80c59d7cd9ebd72704865c1
DARRAGH LALLY <G00220290@gmit.ie>
2018-11-20 10:38:10 +0000

    Add project files.

---
+++ WildCampingApp/WildCampingApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WildCampingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHealth_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btntools_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKnots_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChecklist_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLocation_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
