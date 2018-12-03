920a75dbdf47201d65f1d279be8a594a9f499a7d
DARRAGH LALLY <G00220290@gmit.ie>
2018-12-03 13:16:01 +0000

    Added Flashlight function

--- WildCampingApp/WildCampingApp/ToolsPage.xaml.cs
+++ WildCampingApp/WildCampingApp/ToolsPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+//For flaslight
+using Xamarin.Essentials;
 
 namespace WildCampingApp
 {
@@ -16,5 +18,48 @@ namespace WildCampingApp
 		{
 			InitializeComponent ();
 		}
-	}
+
+        async void btnTorch_Clicked(object sender, EventArgs e)
+        {
+            //Torch code here
+            try
+            {
+                // Turn On
+                await Flashlight.TurnOnAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+
+        async void btnTorch1_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // Turn Off
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+    }
 }
