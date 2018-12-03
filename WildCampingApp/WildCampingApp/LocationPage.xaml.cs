3be0d6a311515c49f4b37e34fdc72d8764766f67
DARRAGH LALLY <G00220290@gmit.ie>
2018-12-03 12:09:32 +0000

    Format Lat and Long output

--- WildCampingApp/WildCampingApp/LocationPage.xaml.cs
+++ WildCampingApp/WildCampingApp/LocationPage.xaml.cs
@@ -30,13 +30,55 @@ namespace WildCampingApp
 
                 if (location != null)
                 {
-                    var lat = location.Latitude;
-                    var lon = location.Longitude;
-                    String latOutput = "Latitude: " + lat;
-                    String lonOutput = "Longitude: " + lon;
-                    //Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
-                    lblLocationDisplay_Lat.Text = latOutput;
-                    lblLocationDisplay_Long.Text = lonOutput;
+
+                    //**Converting GPS Lat to DMS Lat              
+                    // Getting Degrees and Converting minutes
+                    int latDegrees = (int)location.Latitude;
+                    double latDecOne = location.Latitude - latDegrees;
+                    double latMinutesDec = latDecOne * 60;
+                    //Converting seconds and rounding seconds to 2 places
+                    int latMinutes = (int)latMinutesDec;
+                    double latDecTwo = latMinutesDec - latMinutes;
+                    double latSeconds = latDecTwo * 60;
+                    double latSecondsRound = Math.Round(latSeconds, 2, MidpointRounding.AwayFromZero);
+                    //Display compass direction
+                    String compassHeadingLat;
+                    if (location.Latitude > 0)
+                    {
+                        compassHeadingLat = "North";
+                    }
+                    else {
+                        compassHeadingLat = "South";
+                    }
+                    //Display to label
+                    String outputLat ="Latitude: " + Math.Abs(latDegrees) +  " degree's | " + Math.Abs((int)latMinutes) + " minute's | " + Math.Abs(latSecondsRound) + " second's " + compassHeadingLat;
+                    lblLocationDisplay_Lat.Text = outputLat;
+
+                    //**Converting GPS Long to DMS Long              
+                    // Getting Degrees and Converting minutes
+                    int lonDegrees = (int)location.Longitude;
+                    double lonDecOne = location.Longitude - lonDegrees;
+                    double lonMinutesDec = lonDecOne * 60;
+                    //Converting seconds and rounding seconds to 2 places
+                    int lonMinutes = (int)lonMinutesDec;
+                    double lonDecTwo = lonMinutesDec - lonMinutes;
+                    double lonSeconds = lonDecTwo * 60;
+                    double lonSecondsRound = Math.Round(lonSeconds, 2, MidpointRounding.AwayFromZero);
+                    //Display compass direction
+                    String compassHeadingLon;
+                    if (location.Longitude > 0)
+                    {
+                        compassHeadingLon = "East";
+                    }
+                    else
+                    {
+                        compassHeadingLon = "West";
+                    }
+                    //Display to label
+                    String outputLon = "Longitude: " + Math.Abs(lonDegrees) + " degree's | " + Math.Abs((int)lonMinutes) + " minute's | " + Math.Abs(lonSecondsRound) + " second's " + compassHeadingLon;
+                    lblLocationDisplay_Long.Text = outputLon;
+
+
                 }
             }
             catch (FeatureNotSupportedException fnsEx)
